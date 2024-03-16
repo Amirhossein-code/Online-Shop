ae94e3afbaeb7411868d6ee3261fccaa93a1ef03
Amir <edadiamirhossein@gmail.com>
2024-03-16 09:21:59 -0400

    authentication backend updated to support email and password auth

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,10 +1,51 @@
-from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    Group,
+    PermissionsMixin,
+)
+from django.core.validators import validate_email
 from django.db import models
 
 
-# Create your models here.
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    username = None
+class UserManager(BaseUserManager):
+    """
+    User Manager.
+    To create superuser.
+    """
+
+    def create_user(self, email, password=None):
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email=email, password=password)
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom User model.
+    """
+
+    email = models.EmailField(
+        max_length=50, blank=True, null=True, validators=[validate_email], unique=True
+    )
+
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    user_registered_at = models.DateTimeField(auto_now_add=True)
+
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
